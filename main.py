e541f00f46adb049bb5e5f6a469c78011ba3f2be
quetinn <noe.quetin@edu.esiee.fr>
2024-09-16 13:03:04 +0000

    exercice palindrome Noé QUETIN

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
+"""
+
+"""
 #### Fonction secondaire
 
 
 def ispalindrome(p):
+    """
+    retourne un bouléen
 
-    # votre code ici
-    
+    Args:
+        p: chaine de caractères
+
+    """
+    table = str.maketrans("éèêëàïùçô", "eeeeaiuco", "?.:/!', -")
+    p =p.lower().translate(table)
+    if p[::-1]== p:
+        return True
     return False
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    appel fonction palindrome
+    """
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
